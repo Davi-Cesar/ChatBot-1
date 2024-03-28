e5aada6ad3dadcbf20963b3f008a8f46fb1fad68
João Paulo Carneiro <joao.paulo_silva@icloud.com>
2024-03-28 00:00:30 -0300

    Add de alguns arquivos úteis.

--- src/main/java/com/project/chatbot/Mensagem.java
+++ src/main/java/com/project/chatbot/Mensagem.java
@@ -1,21 +1,7 @@
 package com.project.chatbot;
 public class Mensagem {
-    public String usuario;
-    public String mensagem;
+    public String user;
+    public String msg;
 
-    public String getUsuario() {
-        return usuario;
-    }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
 }
